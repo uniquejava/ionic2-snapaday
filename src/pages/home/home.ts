5b18b6609cef42635926a1c478d399dbca814a1b
cyper <345343747@qq.com>
2017-04-05 15:12:23 +0800

    model and SimpleAlert service

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import {NavController} from 'ionic-angular';
+import {SimpleAlert} from "../../providers/simple-alert";
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,8 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, public simpleAlert: SimpleAlert) {
+
   }
 
 }
--- src/providers/simple-alert.ts
+++ src/providers/simple-alert.ts
@@ -1,18 +1,32 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
+import {AlertController} from "ionic-angular";
 
 /*
-  Generated class for the SimpleAlert provider.
+ Generated class for the SimpleAlert provider.
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
+ See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+ for more info on providers and Angular 2 DI.
+ */
 @Injectable()
 export class SimpleAlert {
 
-  constructor(public http: Http) {
+  constructor(public http: Http, public alertCtrl: AlertController) {
     console.log('Hello SimpleAlert Provider');
   }
+
+  createAlert(title: string, message: string) {
+    return this.alertCtrl.create({
+      title: title,
+      message: message,
+      buttons: [
+        {
+          text: 'Ok'
+        }
+      ]
+    });
+  }
+
 
 }
