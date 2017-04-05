0b3244eab51d09bee41dac34c5f1c1925395ad5e
cyper <345343747@qq.com>
2017-04-05 21:32:09 +0800

    add local notifications in root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,41 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component} from '@angular/core';
+import {Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import {HomePage} from '../pages/home/home';
+import {LocalNotifications} from "@ionic-native/local-notifications";
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, localNotifications: LocalNotifications) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      if (platform.is('cordova')) {
+        localNotifications.isScheduled(1).then(scheduled => {
+          if (!scheduled) {
+            let firstNotificationTime = new Date();
+            firstNotificationTime.setHours(firstNotificationTime.getHours() + 24);
+            localNotifications.schedule({
+              id: 1,
+              title: 'Snapaday',
+              text: 'Have you taken your snap today?',
+              at: firstNotificationTime,
+              every: 'day'
+            });
+          }
+        });
+      }
+
     });
   }
 }
